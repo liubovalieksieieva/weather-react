b2174a18f0fb9ccfc51c95b083519ee35cb57bfd
Liubov Alieksieieva <liubovalieksieieva@gmail.com>
2024-05-29 17:50:40 +0300

    Weather react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState({});
+  const [city, setCity] = useState("");
+
+  const key = "553a7b4089a1ea802f48510994124848";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`;
+
+  const searchWeather = (e) => {
+    if (e.key === "Enter") {
+      axios.get(url).then((response) => {
+        setData(response.data);
+      });
+      setCity("");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="input">
+        <input
+          name="city"
+          type="text"
+          placeholder="Location"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          onKeyDown={searchWeather}
+        />
+      </div>
+      <div className="container">
+        <div className="header">
+          <h1>{data.name}</h1>
+          <div>
+            {data.main ? <div>{data.main.temp.toFixed()}°C</div> : null}
+          </div>
+        </div>
+        <div className="main">
+          <div>
+            {data.weather ? <div>{data.weather[0].description}</div> : null}
+          </div>
+          <div className="image">
+            {data.weather ? (
+              <img
+                src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+                alt="weather"
+              />
+            ) : null}
+          </div>
+        </div>
+      </div>
+      <div className="footer">
+        {data.main !== undefined && (
+          <div className="footer-elements">
+            <div>
+              {data.main ? (
+                <span>Feels like {data.main.feels_like.toFixed()}°C</span>
+              ) : null}
+            </div>
+            <div>
+              {data.main ? <span>Humidity {data.main.humidity} %</span> : null}
+            </div>
+            <div>
+              {data.wind ? <span>Wind {data.wind.speed} м/с</span> : null}
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
